c0576d7208e5a9279acd15110769b52b0f7b766f
Giragloss22 <54596991+Giragloss@users.noreply.github.com>
2022-04-14 20:37:08 -0300

    Tamanho do Player Dinâmico

--- Embed/main.js
+++ Embed/main.js
@@ -1,10 +1,12 @@
-const links = JSON.parse(atob(document.URL.split('#')[1].split('?=')[0]))
+let links = JSON.parse(atob(document.URL.split('#')[1].split('?=')[0]))
 const options = {controlBar: {children: ['playToggle', 'progressControl', 'volumePanel', 'captionsButton', 'qualitySelector', 'fullscreenToggle']}}
 
 init()
 async function init() {
   await addResources(links)
+  await setPlayer()
   await optionsSet()
+  await urlObserver()
 }
 
 function addResources(links) {
@@ -22,14 +24,26 @@ function addResources(links) {
   trackSub.setAttribute('src', links.vtt)
   trackSub.setAttribute('srclang', 'pt-br')
   trackSub.setAttribute('label', 'Portugues')
+}
 
+function setPlayer(id) {
   const player = document.querySelector('#player')
+  const playerVideo = document.querySelector('#player video')
   const tamanho = document.URL.split('?=')[1].split('x')
-  player.setAttribute('poster', links.thumb)
+
   player.setAttribute('style', `width: ${tamanho[0]}px; height: ${tamanho[1]}px;`)
+  if(id){
+    playerVideo.setAttribute('style', `width: ${tamanho[0]}px; height: ${tamanho[1]}px;`)
+  }
 }
 
 function optionsSet() {
   videojs('player', options);
   document.querySelector('.vjs-quality-selector').className='vjs-audio-button vjs-menu-button vjs-menu-button-popup vjs-control vjs-button'
+}
+
+function urlObserver(argument) {
+  window.addEventListener('hashchange', function(){
+    setPlayer(1)
+  });
 }

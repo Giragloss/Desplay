2cdb1b7aadff16e5f70d268861d054625ffb6c15
Giragloss22 <54596991+Giragloss@users.noreply.github.com>
2022-04-17 16:29:36 -0300

    Bug fix & Organization

---
+++ Player/embed/main.js
@@ -0,0 +1,50 @@
+let links = JSON.parse(atob(document.URL.split('?=')[1].split('#')[0]))
+const options = {controlBar: {children: ['playToggle', 'progressControl', 'volumePanel', 'captionsButton', 'qualitySelector', 'fullscreenToggle']}}
+
+init()
+async function init() {
+  await addResources(links)
+  await optionsSet()
+  await urlObserver()
+  await setPlayer()
+}
+
+function addResources(links) {
+  const sourceOrg = document.querySelector('#player #org')
+  sourceOrg.setAttribute('src', links.orig)
+  sourceOrg.setAttribute('type', 'video/mp4')
+  sourceOrg.setAttribute('label', 'Original')
+
+  const sourceDub = document.querySelector('#player #dub')
+  sourceDub.setAttribute('src', links.dub)
+  sourceDub.setAttribute('type', 'video/mp4')
+  sourceDub.setAttribute('label', 'Dublado')
+
+  const trackSub = document.querySelector('#player #sub')
+  trackSub.setAttribute('src', links.vtt)
+  trackSub.setAttribute('srclang', 'pt-br')
+  trackSub.setAttribute('label', 'Portugues')
+}
+
+function optionsSet() {
+  videojs('player', options);
+  document.querySelector('.vjs-quality-selector').className='vjs-audio-button vjs-menu-button vjs-menu-button-popup vjs-control vjs-button'
+}
+
+function urlObserver(argument) {
+  window.addEventListener('hashchange', function(){
+    setPlayer(1)
+  });
+}
+
+function setPlayer(id) {
+  const player = document.querySelector('#player')
+  const playerVideo = document.querySelector('#player video')
+  const tamanho = document.URL.split('#')[1].split('x')
+
+  player.setAttribute('style', `width: ${tamanho[0]}px; height: ${tamanho[1]}px;`)
+  if(id){
+    playerVideo.setAttribute('style', `width: 100%; height: 100%;`)
+  }
+  player.setAttribute('poster', links.thumb)
+}
